f630a3308a43e59a19a77df4dee82205fcba6737
tris-ory <tristanory@sfr.fr>
2022-01-17 12:18:31 +0100

    Lot of optimizations

--- assets/js/chifumi.js
+++ assets/js/chifumi.js
@@ -2,20 +2,13 @@
 const playList = ["pierre", "feuille", "ciseaux"];
 const maxRounds = 3;
 
-var roundCount = 0;
-
-var playerChoice = "";
-var robotChoice = "";
-
-var playerScore = 0;
-var robotScore = 0;
-
 // DOM elements I use in the script
-// var btnRock = document.getElementById("pierre");
-// var btnSheet = document.getElementById("feuille");
-// var btnScissors = document.getElementById("ciseaux");
 var selPlayerSelector = document.getElementById("playerSelector");
+
 var btnReset = document.getElementById("reset");
+var cbRenamePlayer = document.getElementById("renamePlayer");
+
+var h2PlayerName = document.getElementById("playerName");
 
 var h3PlayerScore = document.getElementById("playerScore");
 var h3RobotScore = document.getElementById("robotScore");
@@ -28,9 +21,9 @@ var pRobotPlay = document.getElementById("robotPlay");
 // launch the round with the value selected.
 
 selPlayerSelector.addEventListener("input", function (event) {
-    alert(playList[selPlayerSelector.value]+" sélectionné");
-    selPlayerSelector.value="-1";
-})
+    playRound(selPlayerSelector.value);
+    selPlayerSelector.value = "-1";
+});
 
 // If the player click on reset button, reset the play
 btnReset.addEventListener("click", function (event) {
@@ -50,44 +43,24 @@ function initPlay() {
     robotChoice = "";
     playerScore = 0;
     robotScore = 0;
+    if (cbRenamePlayer.checked) {
+        namePlayer();
+        cbRenamePlayer.checked = false;
+    }
 }
 
-// compare(choice1, choice2)
-// Compare selected values from chifumi
-// Return :
-//      1 if choice1 is better than choice2
-//      0 if choice1 is the same than choice2
-//     -1 in other case
+function namePlayer() {
+    var currentName = h2PlayerName.innerHTML;
+    var newName = prompt("Quel est votre nom ?", currentName);
+    if ((typeof newName === "string" || newName instanceof String) && newName != "") {
+        h2PlayerName.innerText = newName;
+    }
+}
 
-// function compare(choice1, choice2) {
-//     if (choice1 === "pierre") {
-//         if (choice2 === "pierre") {
-//             return 0;
-//         } else if (choice2 === "feuille") {
-//             return -1;
-//         } else if (choice2 === "ciseaux") {
-//             return 1;
-//         }
-//     } else if (choice1 === "feuille") {
-//         if (choice2 === "pierre") {
-//             return 1;
-//         } else if (choice2 === "feuille") {
-//             return 0;
-//         } else if (choice2 === "ciseaux") {
-//             return -1;
-//         }
-//     } else if (choice1 === "ciseaux") {
-//         if (choice2 === "pierre") {
-//             return -1;
-//         } else if (choice2 === "feuille") {
-//             return 1;
-//         } else if (choice2 === "ciseaux") {
-//             return 0;
-//         }
-//     }
-
-//     console.log("Erreur ! au moins un des paramètres est invalide !");
-//}
+// Return random play for robot
+function robotPlay() {
+    return parseInt(Math.random() * 3);
+}
 
 // compare(choice1, choice2)
 // Compare selected values from chifumi
@@ -96,22 +69,7 @@ function compare(choice1, choice2) {
     if (choice1 === choice2) {
         return "egalite";
     }
-    if (choice1 === "pierre") {
-        if (choice2 === "feuille") {
-            return choice2;
-        } else {
-            return choice1;
-        }
-    }
-    if (choice1 === "feuille") {
-        if (choice2 === "ciseaux") {
-            return choice2;
-        } else {
-            return choice1;
-        }
-    }
-    // If choice1 is not "pierre" nor "ciseaux", it's necessarily "feuille", so no if for this case
-    if (choice2 === "pierre") {
+    if (choice2 % 3 == (choice1 + 1) % 3) {
         return choice2;
     } else {
         return choice1;
@@ -119,10 +77,6 @@ function compare(choice1, choice2) {
 }
 
 
-// Return random play for robot
-function robotPlay() {
-    return playList[parseInt(Math.random() * 3)];
-}
 
 // Play a round 
 function playRound(playerChoice) {
@@ -130,18 +84,21 @@ function playRound(playerChoice) {
         roundCount++;
         robotChoice = robotPlay();
         var roundResult = compare(playerChoice, robotChoice);
+        if (roundResult != "égalité"){
+            
         if (roundResult == playerChoice) {
             playerScore++;
         } else if (roundResult == robotChoice) {
             robotScore++;
         }
+        }
         // Print new scores
         h3PlayerScore.innerHTML = playerScore;
         h3RobotScore.innerHTML = robotScore;
         // and last played
-        pPlayerPlay.innerHTML = "Vous avez joué " + playerChoice;
-        pRobotPlay.innerHTML = "Le robot a joué " + robotChoice;
-        // After
+        pPlayerPlay.innerHTML = "Vous avez joué " + playList[playerChoice];
+        pRobotPlay.innerHTML = "Le robot a joué " + playList[robotChoice];
+        // After last round
         if (roundCount >= maxRounds) {
             var msg = "";
             if (playerScore > robotScore) {
@@ -157,3 +114,6 @@ function playRound(playerChoice) {
         alert("La partie est terminée, cliquez sur le bouton nouvelle partie pour recommencer");
     }
 }
+
+// Initialize the game
+initPlay();
